a275d7fd4a1248df58d70425226bb9b5f79e82f4
Misa Liu <misaliu@misaliu.top>
2024-03-18 16:16:12 +0800

    chore: Add `Vite`

---
+++ electron.vite.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from 'electron-vite';
+import { defineConfig as defineViteConfig } from 'vite';
+import { resolve } from 'path';
+import viteCp from 'vite-plugin-cp';
+import viteZipPack from 'unplugin-zip-pack/vite';
+import PluginManifest from './manifest.json';
+
+const SRC_DIR = resolve(__dirname, './src');
+const OUTPUT_DIR = resolve(__dirname, './dist');
+
+const BaseConfig = defineViteConfig({
+  root: __dirname,
+  resolve: {
+    alias: {
+      '@': SRC_DIR,
+    },
+  },
+});
+
+const ConfigBuilder = (type: 'main' | 'preload') => defineViteConfig({
+  ...BaseConfig,
+
+  build: {
+    minify: true,
+    outDir: resolve(OUTPUT_DIR, `./${type}`),
+    lib: {
+      entry: resolve(SRC_DIR, `./${type}/index.ts`),
+      formats: [ 'cjs' ],
+      fileName: () => 'index.js',
+    },
+  },
+});
+
+export default defineConfig({
+  main: ConfigBuilder('main'),
+  preload: ConfigBuilder('preload'),
+  renderer: defineViteConfig({
+    ...BaseConfig,
+
+    plugins: [
+      viteCp({
+        targets: [{ src: './manifest.json', dest: 'dist' }],
+      }),
+      viteZipPack({
+        in: OUTPUT_DIR,
+        out: resolve(__dirname, `./${PluginManifest.slug}.zip`),
+      }),
+    ],
+    build: {
+      minify: 'esbuild',
+      outDir: resolve(OUTPUT_DIR, './renderer'),
+      lib: {
+        entry: resolve(SRC_DIR, './renderer/index.ts'),
+        formats: [ 'es' ],
+        fileName: () => 'index.js',
+      },
+      rollupOptions: {
+        input: resolve(SRC_DIR, './renderer/index.ts'),
+      },
+    },
+  }),
+});
